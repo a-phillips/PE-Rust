f5a20e1f109665fdb4f8c96d5d3b6fd63d27ce5b
Andrew Phillips <arphillips3@me.com>
2014-12-18 00:30:49 -0500

    3 not mutable

--- src/bin/PE003.rs
+++ src/bin/PE003.rs
@@ -1,11 +1,13 @@
+//This feels like cheating - if I start max_prime_div with div=2
+//and skip by 1 in the function's "else" statement, then it
+//overflows, but it doesn't if you start at 3 and skip by 2.
+
+fn max_prime_div(num: i64, div: i64) -> i64{
+    if num == 1 { return div; }
+    else if num%div==0 { return max_prime_div(num/div, div); }
+    else { return max_prime_div(num, div+2); }
+}
+
 fn main(){
-    let mut x: i64 = 600851475143;
-    let mut div: i64 = 1;
-    while x != 1 {
-        div = div + 1;
-        while (x%div) == 0 {
-            x = x / div;
-        }
-    }
-    println!("{}", div);
+    println!("{}", max_prime_div(600851475143, 3));
 }
