df2eb870fca30a17251f83a326949e4038fef731
Andrew Phillips <arphillips3@me.com>
2014-12-18 01:24:56 -0500

    6 not mutable

--- src/bin/PE006.rs
+++ src/bin/PE006.rs
@@ -1,12 +1,16 @@
+use std::num::Int;
+
+fn get_sum_sq(lim: int, i: int) -> int{
+    if i > lim { return 0; }
+    else { return (i*i) + get_sum_sq(lim, i+1); }
+}
+
+fn get_sq_sum(lim: int, i: int) -> int{
+    if i > lim { return 0; }
+    else if i != 1 { return i + get_sq_sum(lim, i+1); }
+    else { return (i + get_sq_sum(lim, i+1)).pow(2); }
+}
+
 fn main(){
-    let mut SumSq: i64 = 0;
-    let mut SqSum: i64 = 0;
-    for i in range(1, 101){
-        SumSq += i*i;
-        SqSum += i;
-    }
-    SqSum = SqSum*SqSum;
-    println!("{}",SqSum);
-    println!("{}",SumSq);
-    println!("{}",SqSum-SumSq);
+    println!("{}",get_sq_sum(100, 1) - get_sum_sq(100, 1));
 }
