9b1a7d3d50dfae997d08bd4ba2696bbfe2d15701
Andrew Phillips <arphillips3@me.com>
2014-12-29 10:40:12 -0500

    revised 7

--- src/bin/PE007.rs
+++ src/bin/PE007.rs
@@ -1,12 +1,33 @@
 //I don't like this brute-force solution - a sieve of sorts is for sure faster
 use std::num::Float;
 
-fn is_prime(num:i64) -> bool {
-	let lim = ((num as f64).sqrt() as i64) + 1;
-	for i in range(2i64, lim) {
-		if num % i == 0 { return false; }
+//EDIT: below is the original is_prime function:
+//fn is_prime(num:i64) -> bool {
+//	let lim = ((num as f64).sqrt() as i64) + 1;
+//	for i in range(2i64, lim) {
+//		if num % i == 0 { return false; }
+//	}
+//	return true;
+//}
+//
+//I'm replacing that with this faster function which uses the fact that all primes > 3 can be written
+//as 6k +/- 1 (although some, like 6(4)+1=25, are not prime, but we don't know when that will be)
+//This runs *significantly* faster than the original.
+
+fn is_prime (num:i64) -> bool {
+	//The below statements take care of numbers divisible by 2 or 3, which is most
+	if num < 2 { return false; }
+	else if num < 4 { return true; }
+	else if (num%2==0) || (num%3==0) { return false; }
+	else {
+		let mut factor = 5i64; //6(1)-1
+		let lim = ((num as f64).sqrt() as i64)+1;
+		while factor < lim {
+			if (num%factor==0) || (num%(factor+2)==0) { return false; } //divisible by 6k-1 or 6k+1
+			factor += 6;
+		}
+		return true;
 	}
-	return true;
 }
 
 fn main() {
