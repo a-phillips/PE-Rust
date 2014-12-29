cd9b8b7de1a72b46c5c7406f62cd83e454747fa3
Andrew Phillips <arphillips3@me.com>
2014-12-29 17:39:20 -0500

    21

---
+++ src/bin/PE021.rs
@@ -0,0 +1,22 @@
+//naive, but it works.
+
+fn d(num:uint) -> uint {
+	range(1u, num).filter(|x| num%*x==0).fold(0u, |acc, i| {acc + i})
+}
+
+fn main() {
+	let mut d_arr = [0u, ..10_000];
+	let mut d1:uint;
+	let mut d2:uint;
+	let mut sum = 0u;
+	for i in range(2u, 10_000) {
+		d_arr[i] = d(i);
+	}
+	for i in range(2u, 10_000) {
+		if (d_arr[i] < 10000) && (i == d_arr[d_arr[i]]) && (i != d_arr[i]) {
+			println!("{}", i);
+			sum += i;
+		}
+	}
+	println!("{}", sum);
+}
